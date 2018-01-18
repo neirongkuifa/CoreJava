4069c892985af190e362aae702b02be93c2aeccd
Wen Jiang <abeljiang@hotmail.com>
2018-01-17 19:26:40 -0800

    Finish

--- InnerClassTest.java
+++ InnerClassTest.java
@@ -8,7 +8,6 @@ import java.util.*;
 public class InnerClassTest{
     public static void main(String...args){
         TalkingClock tc=new TalkingClock(1000, true);
-        System.out.println(tc);
         tc.start();
         // ActionListener tp=tc.new TimePrinter();
         // Timer t=new Timer(1000, tp);
@@ -29,17 +28,25 @@ class TalkingClock{
     }
 
     public void start(){
+        int test=5;
+        // test=8;
+        class localPrinter implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Local Inner Class:"+this.toString()+" "+TalkingClock.this.interval+" "+test);
+            }
+        }
+        //int test=5;
         Timer t=new Timer(interval, new TimePrinter());
         Timer t1=new Timer(interval, new PrinterTest(this));
-        t.start();
-        t1.start();
+        Timer t2=new Timer(interval, new localPrinter());
+        // t.start();
+        // t1.start();
+        t2.start();  
     }
 
     public class TimePrinter implements ActionListener{
         public void actionPerformed(ActionEvent e){
-            System.out.println("Inner Class: At the tone, the time is "+new Date());
-            System.out.println(this.toString());
-            if(beep) Toolkit.getDefaultToolkit().beep();
+            System.out.println("Inner Class:"+this.toString());
         }
     }
 }
@@ -47,12 +54,9 @@ class TalkingClock{
 class PrinterTest implements ActionListener{
     private boolean beep;
     public PrinterTest(TalkingClock tc){
-        System.out.println(tc.toString());
         this.beep=true;
     }
     public void actionPerformed(ActionEvent e){
-        System.out.println("Regular Class: At the tone, the time is "+new Date());
-        System.out.println(this.toString());
-        if(beep) Toolkit.getDefaultToolkit().beep();
+        System.out.println("Regular Class:"+this.toString());
     }
 }
