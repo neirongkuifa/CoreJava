c9e45a8cde4b20d06f60b2d342c8a73670dce11a
Wen Jiang <abeljiang@hotmail.com>
2018-01-29 17:35:56 -0800

    Create Exception Test

---
+++ Company/ExceptionTest.java
@@ -0,0 +1,12 @@
+public class ExceptionTest{
+    public static void main(String...args) throws Throwable{
+        try{
+            System.out.println("Under this line, there will be an exception: ");
+            throw new Throwable("Here is the exception!");
+        }catch(Throwable t){
+            System.out.println(t.getMessage());
+        }
+        throw new Throwable("This is an exception test!");
+    }
+    
+}
